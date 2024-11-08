91379bcf1714cd3297ddebb51ebf5cd7e38d4ac3
daxxn <dahyeonhan4529@gmail.com>
2024-11-09 00:38:53 +0900

    feat(util): add file reader util
    - Implement FileReaderUtil class to handle read md file

---
+++ src/main/java/Util/FileReaderUtil.java
@@ -0,0 +1,26 @@
+package Util;
+
+import error.CustomException;
+import error.ExceptionMessage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileReaderUtil {
+    public static String DELIMITER = ",";
+
+    public static List<String> readFile(String filePath) {
+        try {
+            return Files.readAllLines(Paths.get(filePath));
+        } catch (IOException e) {
+            throw new CustomException(ExceptionMessage.ERROR_MESSAGE_CANNOT_READ_FILE);
+        }
+    }
+
+    public static List<String> delimiterLine(String line) {
+        List<String> list;
+        list = List.of(line.split(DELIMITER));
+        return list;
+    }
+}
