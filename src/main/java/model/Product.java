3894b2daf7ad0047a1d38e4d57a004a5e1219779
daxxn <dahyeonhan4529@gmail.com>
2024-11-11 00:01:30 +0900

    feat(model): update product class methods

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -5,7 +5,7 @@ public class Product {
     private int quantity;
     private final int price;
     private final String promotion;
-
+    static final String NULL_PROMOTION = "null";
 
     public Product(String name, int quantity, int price, String promotion) {
         this.name = name;
@@ -28,5 +28,35 @@ public class Product {
 
     public int getQuantity() {
         return this.quantity;
+    }
+
+    public String getPromotion() {
+        return this.promotion;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String makeQuantityMessage() {
+        if (this.quantity == 0) {
+            return " 재고 없음";
+        }
+        return " " + this.quantity + "개";
+    }
+
+    public String makePromotionMessage() {
+        if (!this.promotion.equals(NULL_PROMOTION)) {
+            return " " + this.promotion;
+        }
+        return "";
+    }
+
+    public String getProductStockInfo() {
+        String stockInfo = "- "
+                + this.name + " " + String.format("%,d", this.price) + "원"
+                + makeQuantityMessage()
+                + makePromotionMessage();
+        return stockInfo;
     }
 }
