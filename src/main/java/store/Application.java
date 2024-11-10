50d09b7a957c0faf8c396a17e279d407e8ecba82
daxxn <dahyeonhan4529@gmail.com>
2024-11-11 02:39:08 +0900

    feat(app): add application class to start order processing
    - Implement Application class to initialize and run the order process via OrderController

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,10 @@
 package store;
 
+import controller.OrderController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OrderController orderController = new OrderController();
+        orderController.run();
     }
 }
